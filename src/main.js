33ed5b8a456fd2432264c496c41bfe31cfec1908
Ismael <ismaeldom11@gmail.com>
2021-01-07 15:47:03 -0600

    Se importan componente de PrimeVue.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import InputNumber from 'primevue/inputnumber';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import InputText from 'primevue/inputtext';
 
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
@@ -12,5 +15,8 @@ const app = createApp(App);
 app.use(PrimeVue);
 
 app.component('InputNumber', InputNumber);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('InputText', InputText);
 
 app.mount('#app');
