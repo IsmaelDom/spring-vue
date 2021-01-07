fbee56bc19bd115f4be4fcc0a86d24444fb3178c
Ismael <ismaeldom11@gmail.com>
2021-01-07 00:11:23 -0600

    Se agregan estilos y componentes de primeVue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import InputNumber from 'primevue/inputnumber';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('InputNumber', InputNumber);
+
+app.mount('#app');
