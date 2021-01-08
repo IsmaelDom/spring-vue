01b372d12db37309829d73ce2ea5cf3cb3a114ae
Ismael <ismaeldom11@gmail.com>
2021-01-08 10:03:27 -0600

    Se agrega una nueva ruta y se ralizan cambios en la vista Nuevo.

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Nuevo from '../views/Direccion/Nuevo.vue'
 
+//Rutas programadas
 const routes = [
   {
-    path: '/',
+    path: '/',//URL
     name: 'Home',
-    component: Home
+    component: Home //Componente a usar
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/registrar',//URL
+    name: 'Nuevo',
+    component: Nuevo
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(process.env.BASE_URL),//URL base
+  routes  //Se pasan las rutas
 })
 
 export default router
---
+++ src/views/Direccion/Nuevo.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+    <h1>Nueva Dirección</h1>
+    <div class="p-fluid p-grid p-formgrid">
+        <div class="p-field p-col-12 p-md-4">
+            <span class="p-float-label">
+                <InputText type="text" id="nombre"/>
+                <label for="nombre">Nombre:</label>
+            </span>
+        </div>
+        <div class="p-field p-col-12 p-md-4">
+        <span class="p-float-label">
+                <InputText type="text" placeholder="Ingrese apellido"/>
+                <label for="nombre">Apellido:</label>
+            </span>
+        </div>
+        <div class="p-field p-col-12 p-md-4">
+            <InputText type="text" placeholder="Ingrese un correo" v-tooltip.bottom.focus="'Ingrese su E-mail'" />
+        </div>
+        <div class="p-field p-col-12 p-md-4">
+            <InputText type="text" placeholder="Ingrese edad" v-tooltip.bottom.focus="'Ingrese su edad'" />
+        </div>
+        <div class="p-field p-col-12 p-md-4">
+            <InputText type="text" placeholder="Ingrese calle" v-tooltip.bottom.focus="'Ingrese su calle'" />
+        </div>
+        <div class="p-field p-col-12 p-md-4">
+            <InputText type="text" placeholder="Ingrese su código postal"/>
+        </div>
+        <div class="p-field p-col-12 p-md-4">
+            <InputText type="text" placeholder="Ingrese su estado" v-tooltip.bottom.focus="'Ingrese su estado'" />
+        </div>
+        <div class="p-field p-col-12 p-md-4">
+            <InputText type="text" placeholder="Ingrese su municipio" v-tooltip.bottom.focus="'Ingrese su municipio'" />
+        </div>
+        <div class="p-field p-col-12 p-md-4">
+            <InputText type="text" placeholder="Ingrese su no. exterior"/>
+        </div>
+        <div class="p-field p-col-12 p-md-4">
+            <InputText type="text" placeholder="Ingrese una referencia"/>
+        </div>
+    </div>
+    <Button type="button" label="Guardar" icon="pi pi-check" v-tooltip="'Click para guardar'" />
+    <Button type="button" label="Regresar" icon="pi pi-check" v-tooltip="'Click para regresar'" />
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    },
+    
+}
+</script>
+
+<style>
+</style>
