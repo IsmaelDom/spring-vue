09bd492c80b23b9bcfe6f8dfd1eba8d71cd70dc2
Ismael <ismaeldom11@gmail.com>
2021-01-08 16:16:01 -0600

    Se redirecciona a la vista registrar.

--- src/components/Direccion/Boton.vue
+++ src/components/Direccion/Boton.vue
@@ -18,6 +18,7 @@ export default {
                         {
                             label: 'Nuevo',
                             icon: 'pi pi-user-plus',
+                            to: '/registrar'
                         },
                         {
                             label: 'Editar',
