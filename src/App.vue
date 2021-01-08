696252a20511dddd56a6be8415990acea4bb4e97
Ismael <ismaeldom11@gmail.com>
2021-01-07 21:18:16 -0600

    Al agregar vue-router se crearon nuevas carpetas y archivos.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <RestDireccion/>
+  <router-view/>
 </template>
-
-<script>
-import RestDireccion from './components/Direccion/RestDireccion.vue'
-
-export default {
-  name: 'App',
-  components: {
-    RestDireccion
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +9,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <RestDireccion></RestDireccion>
+</template>
+
+<script>
+import RestDireccion from '../components/Direccion/RestDireccion.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RestDireccion
+  }
+}
+</script>
