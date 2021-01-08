820d53a3e0e7d33d70c07d6b96a56405eaf82af6
Ismael <ismaeldom11@gmail.com>
2021-01-08 15:44:51 -0600

    Se guardan los datos en cada variable y se muestran por consola los datos ingresados.

--- src/views/Direccion/Nuevo.vue
+++ src/views/Direccion/Nuevo.vue
@@ -4,68 +4,71 @@
         <div class="p-fluid p-grid">
             <div class="p-field p-col-12 p-md-4">
                 <span class="p-float-label">
-                    <InputText type="text" id="nombre"/>
+                    <InputText type="text" id="nombre" v-model="direccion.usuario.nombre"/>
                     <label for="nombre">Nombre:</label>
                 </span>
             </div>
             <div class="p-field p-col-12 p-md-4">
                 <span class="p-float-label">
-                    <InputText type="text" id="apellido" />
+                    <InputText type="text" id="apellido" v-model="direccion.usuario.apellido"/>
                     <label for="apellido">Apellido:</label>
                 </span>
             </div>
             <div class="p-field p-col-12 p-md-4">
                 <span class="p-float-label">
-                    <InputText type="text" id="correo" />
+                    <InputText type="text" id="correo" v-model="direccion.usuario.correo"/>
                     <label for="correo">Correo:</label>
                 </span>
             </div>
             <div class="p-field p-col-12 p-md-2">
                 <span class="p-float-label">
-                    <InputNumber :min="0" :max="120" id="edad" mode="decimal" showButtons/>
+                    <InputNumber :min="0" :max="120" id="edad" mode="decimal" showButtons v-model="direccion.usuario.edad"/>
                     <label for="edad">Edad:</label>
                 </span>
             </div>
-            <div class="p-field p-col-12 p-md-4">
+            <div class="p-field p-col-12 p-md-3">
+                <Password id="pass" v-model="direccion.usuario.password" placeholder="Ingrese una contraseña"/>
+            </div>
+            <div class="p-field p-col-12 p-md-3">
                 <span class="p-float-label">
-                    <InputText type="text" id="calle" />
+                    <InputText type="text" id="calle" v-model="direccion.calle"/>
                     <label for="calle">Calle:</label>
                 </span>
             </div>
             <div class="p-field p-col-12 p-md-2">
                 <span class="p-float-label">
-                    <InputText id="cp" type="text"/>
+                    <InputText id="cp" type="text" v-model="direccion.cp"/>
                     <label for="cp">Código Postal:</label>
                 </span>
             </div>
-            <div class="p-field p-col-12 p-md-4">
+            <div class="p-field p-col-12 p-md-2">
                 <span class="p-float-label">
-                    <InputText type="text" id="estado"/>
-                    <label for="estado">Estado:</label>
-                </span>
-            </div>
-            <div class="p-field p-col-12 p-md-4">
-                <span class="p-float-label">
-                    <InputText type="text" id="municipio"/>
-                    <label for="municipio">Municipio:</label>
-                </span>
-            </div>
-            <div class="p-field p-col-12 p-md-4">
-                <span class="p-float-label">
-                    <InputNumber :min="0" id="no_exterior"/>
+                    <InputNumber :min="0" id="no_exterior" mode="decimal" showButtons v-model="direccion.no_exterior"/>
                     <label for="no_exterior">No. Exterior:</label>
                 </span>
             </div>
             <div class="p-field p-col-12 p-md-4">
                 <span class="p-float-label">
-                    <InputText type="text" id="referencia"/>
+                    <InputText type="text" id="estado" v-model="direccion.estado"/>
+                    <label for="estado">Estado:</label>
+                </span>
+            </div>
+            <div class="p-field p-col-12 p-md-4">
+                <span class="p-float-label">
+                    <InputText type="text" id="municipio" v-model="direccion.municipio"/>
+                    <label for="municipio">Municipio:</label>
+                </span>
+            </div>
+            <div class="p-field p-col-12 p-md-4">
+                <span class="p-float-label">
+                    <InputText type="text" id="referencia" v-model="direccion.referencia"/>
                     <label for="referencia">Referencia:</label>
                 </span>
             </div>
         </div>
         <div class="p-mb-2">
-            <Button type="button" label="Guardar" icon="pi pi-check" class="p-mr-2 p-mb-2" v-tooltip="'Click para guardar'" />
-            <Button type="button" label="Regresar" icon="pi pi-arrow-left" class="p-mb-2 p-button-danger" v-tooltip="'Click para regresar'" />
+            <Button type="button" label="Guardar" @click="guardar()" icon="pi pi-check" class="p-mr-2 p-mb-2" v-tooltip.bottom="'Click para guardar'" />
+            <Button type="button" label="Regresar" icon="pi pi-arrow-left" class="p-mb-2 p-button-danger" v-tooltip.bottom="'Click para regresar'" />
         </div>
     </Panel>
 </div>
@@ -75,9 +78,28 @@
 export default {
     data(){
         return{
-            valor: null,
+            direccion: {
+                    calle: null,
+                    cp: null,
+                    estado: null,
+                    municipio: null,
+                    no_exterior: null,
+                    referencia: null,
+                    usuario:{
+                        nombre: null,
+                        apellido: null,
+                        password: null,
+                        correo: null,
+                        edad: null,
+                    },
+                },
         }
     },
+    methods:{
+        guardar(){
+            console.log(this.direccion);
+        },
+    }
     
 }
 </script>
