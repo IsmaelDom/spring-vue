92d3811d4d35bb57391e6980cdf5145dced0e93b
Ismael <ismaeldom11@gmail.com>
2021-01-07 21:19:06 -0600

    Se importan elementos y tambien se creo un nuevo archivo con vue-router.

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import InputText from 'primevue/inputtext';
 import Fieldset from 'primevue/fieldset';
+import Menubar from 'primevue/menubar';
+import Boton from './components/Direccion/Boton.vue';
 
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
+import router from './router'
 
-const app = createApp(App);
+const app = createApp(App).use(router);
 
 app.use(PrimeVue);
 
@@ -20,5 +23,7 @@ app.component('DataTable', DataTable);
 app.component('Column', Column);
 app.component('InputText', InputText);
 app.component('Fieldset', Fieldset);
+app.component('Menubar', Menubar);
+app.component('Boton', Boton);
 
 app.mount('#app');
