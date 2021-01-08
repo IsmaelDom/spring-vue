1d96a8f3fc50bd16b7e7d318785ddd320cacab5a
Ismael <ismaeldom11@gmail.com>
2021-01-07 21:20:09 -0600

    Se crea el componente boton y se agregan metodos ora editar, guardar y eliminar.

---
+++ src/components/Direccion/Boton.vue
@@ -0,0 +1,49 @@
+<template>
+    <Menubar :model="items">
+        <template #start>
+        
+        </template>
+        <template #end>
+        
+        </template>
+    </Menubar>
+</template>
+
+<script>
+export default {
+    name: 'Boton',
+        data() {
+            return {
+                items: [
+                        {
+                            label: 'Nuevo',
+                            icon: 'pi pi-fw pi-video',
+                        },
+                        {
+                            label: 'Editar',
+                            icon: 'pi pi-fw pi-users',
+                        },
+                        {
+                            label: 'Eliminar',
+                            icon: 'pi pi-fw pi-trash',
+                        },
+                        {
+                            label: 'Cerrar Sesión',
+                            icon: 'pi pi-fw pi-cog',
+                            command: () => {
+                                this.logout();
+                            }
+                        }
+                ]
+            }
+        },
+        methods: {
+            logout(){
+                console.log("Método logout")
+            }
+        }
+}
+</script>
+
+<style>
+</style>
--- src/services/DireccionService.js
+++ src/services/DireccionService.js
@@ -4,7 +4,26 @@ export default class DireccionService{
     url = 'http://localhost:8080/direcciones/';
 
     getAll(){
-        console.log("Llego al js");
+        console.log("Método getAll()");
         return axios.get(this.url);
+    }
+
+    guardar(direccion){
+        console.log("Método guardar(), direccion:");
+        console.warn(direccion);
+        return axios.post(this.url, direccion);
+    }
+
+    editar(direccion){
+        console.log("Método editar(), direccion:");
+        console.warn(direccion);
+        console.warn(direccion.id);
+        return axios.put(this.url + direccion.id, direccion);
+    }
+
+    eliminar(id){
+        console.log("Método eliminar(), id:");
+        console.warn(id);
+        return axios.delete(this.url + id);
     }
 }
