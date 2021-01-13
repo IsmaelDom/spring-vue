834c610837a74d9f29a8e8f13472a3706c33f595
Ismael <ismaeldom11@gmail.com>
2021-01-12 19:55:24 -0600

    Se agrega catch para mostrar errores.

--- src/service/DireccionService.js
+++ src/service/DireccionService.js
@@ -11,7 +11,13 @@ export default class DireccionService{
 
     getById(id){
         console.log("Método getById()");
-        return axios.get(this.url + '/user/'+id);
+        return axios.get(this.url + '/user/'+id)
+                    .catch(function(error){
+                        if(error.response){
+                            console.error(error.response.headers);
+                            console.error(error.response.status);
+                        }
+                    });
     }
 
     guardar(direccion){
@@ -30,17 +36,35 @@ export default class DireccionService{
         console.log("Método editar(), direccion:");
         console.warn(direccion);
         console.warn(direccion.id);
-        return axios.put(this.url + direccion.id, direccion);
+        return axios.put(this.url + direccion.id, direccion)
+                    .catch(function(error){
+                        if(error.response){
+                            console.error(error.response.headers);
+                            console.error(error.response.status);
+                        }
+                    });
     }
 
     eliminar(id){
         console.log("Método eliminar(), id:");
         console.warn(id);
-        return axios.delete(this.url + id);
+        return axios.delete(this.url + id)
+                    .catch(function(error){
+                        if(error.response){
+                            console.error(error.response.headers);
+                            console.error(error.response.status);
+                        }
+                    });
     }
 
     login(usuario){
         console.warn(usuario);
-        return axios.post(this.urlUser + 'api/login', usuario);
+        return axios.post(this.urlUser + 'api/login', usuario)
+                    .catch(function(error){
+                        if(error.response){
+                            console.error(error.response.headers);
+                            console.error(error.response.status);
+                        }
+                    });
     }
 }
