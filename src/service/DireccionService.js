8d10a00bac9ba71a2c3a8a61e729291e2a6d99a6
Ismael <ismaeldom11@gmail.com>
2021-01-13 12:45:34 -0600

    Se agrega metodo para logout y se agregan las cabeceras a los métodos.

--- src/service/DireccionService.js
+++ src/service/DireccionService.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
+import authHeader from './Header';
 
 export default class DireccionService{
     url = 'http://localhost:8080/direcciones/';
-    urlUser = 'http://localhost:8080/users/';
+    login_url = 'http://localhost:8080/auth/';
 
     getAll(){
         console.log("Método getAll()");
+        headers: authHeader();
         return axios.get(this.url);
     }
 
     getById(id){
         console.log("Método getById()");
+        headers: authHeader();
         return axios.get(this.url + '/user/'+id)
                     .catch(function(error){
                         if(error.response){
@@ -23,6 +26,7 @@ export default class DireccionService{
     guardar(direccion){
         console.log("Método guardar(), direccion:");
         console.warn(direccion);
+        headers: authHeader();
         return axios.post(this.url, direccion)
                     .catch(function(error){
                         if(error.response){
@@ -36,6 +40,7 @@ export default class DireccionService{
         console.log("Método editar(), direccion:");
         console.warn(direccion);
         console.warn(direccion.id);
+        headers: authHeader();
         return axios.put(this.url + direccion.id, direccion)
                     .catch(function(error){
                         if(error.response){
@@ -48,6 +53,7 @@ export default class DireccionService{
     eliminar(id){
         console.log("Método eliminar(), id:");
         console.warn(id);
+        headers: authHeader();
         return axios.delete(this.url + id)
                     .catch(function(error){
                         if(error.response){
@@ -59,12 +65,17 @@ export default class DireccionService{
 
     login(usuario){
         console.warn(usuario);
-        return axios.post(this.urlUser + 'api/login', usuario)
+        headers: authHeader();
+        return axios.post(this.login_url + 'login', usuario)
                     .catch(function(error){
                         if(error.response){
                             console.error(error.response.headers);
                             console.error(error.response.status);
                         }
                     });
+    }
+
+    logout() {
+        localStorage.removeItem('user');
     }
 }
